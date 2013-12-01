63dbcf85e4225780e99d5dff0f6afe24e64333e2
araftery <andrewraftery@gmail.com>
2013-12-01 04:45:16 -0500

    fix some stuff

--- add_test_elements.py
+++ add_test_elements.py
@@ -4,7 +4,7 @@ import datetime
 
 posts = [
 		['Infinite Headbands (B.O.B. ft. 2 Chainz vs. Baauer & RL Grime)', 'https://soundcloud.com/quizzicalproductions/infinite-headbands-b-o-b-ft-2']
-		["Can't Hold Us Can't Stop Us (Macklemore vs Miley Cyrus Mashup) by devcam22", 'https://soundcloud.com/devcam/cant-hold-us-cant-stop-us']
+		["Can't Hold Us Can't Stop Us (Macklemore vs Miley Cyrus Mashup) by devcam22", 'https://soundcloud.com/devcam/cant-hold-us-cant-stop-us'],
 		["If You Ain't First, You're Last (Van Halen x Imagine Dragons x John Mayer x Eminem....) by FBG$", 'https://soundcloud.com/fbgmoney-1/if-you-aint-first-youre-last'],
 		['Best I Ever Had (matamatics remix) by D.Drizzy', 'https://soundcloud.com/d-drizzy-1/best-i-ever-had-matamatics'],
 		['Touch the Sky (matamatics remix) by matamaticsmusic', 'https://soundcloud.com/matamaticsmusic/touch-the-sky-matamatics-remix']
