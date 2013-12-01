f89319ba51904798bb1d8f58f9b3c0610fefcbd1
araftery <andrewraftery@gmail.com>
2013-12-01 04:48:28 -0500

    fix some other stuff

--- add_test_elements.py
+++ add_test_elements.py
@@ -8,15 +8,16 @@ posts = [
 		["If You Ain't First, You're Last (Van Halen x Imagine Dragons x John Mayer x Eminem....) by FBG$", 'https://soundcloud.com/fbgmoney-1/if-you-aint-first-youre-last'],
 		['Best I Ever Had (matamatics remix) by D.Drizzy', 'https://soundcloud.com/d-drizzy-1/best-i-ever-had-matamatics'],
 		['Touch the Sky (matamatics remix) by matamaticsmusic', 'https://soundcloud.com/matamaticsmusic/touch-the-sky-matamatics-remix']
+		]
 
 for i in posts:
 	title = i[0]
 	url = i[1]
 
-    post = Post(title = title, content = '', song_url = url, timestamp = int(datetime.datetime.utcnow().strftime("%s")), author_id = 1)
-    db.session.add(post)
+	post = Post(title = title, content = '', song_url = url, timestamp = int(datetime.datetime.utcnow().strftime("%s")), author_id = 1)
+	db.session.add(post)
 
-    upvote = Upvote(voter_id = 1, post = post)
-    db.session.add(upvote)
+	upvote = Upvote(voter_id = 1, post = post)
+	db.session.add(upvote)
 
 db.session.commit()
