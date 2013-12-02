7da53da7520f71cff11e735ecb2bf7d264a09690
araftery <andrewraftery@gmail.com>
2013-12-01 19:00:59 -0500

    hosting

--- app/views.py
+++ app/views.py
@@ -22,20 +22,6 @@ def index(page = 1):
         posts = posts,
         posts_per_page = POSTS_PER_PAGE)
 
-##############
-# Login view #
-##############
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', 
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
-
 ##################
 # Login Handlers #
 ##################
