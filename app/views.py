5bffd0d1eaf5274c815748ed74b44bb33d35810d
araftery <andrewraftery@gmail.com>
2013-12-04 20:50:42 -0500

    fix datetime bug

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from forms import LoginForm
-from datetime
+import datetime
 from forms import LoginForm, EditForm, PostForm
 from models import *
 from config import POSTS_PER_PAGE
